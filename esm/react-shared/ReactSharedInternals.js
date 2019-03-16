f5401d23d552225c4243673d83d7a8dbe48c3a46
David Burles <dburles@gmail.com>
2019-03-16 12:20:21 +1100

    fix reference

--- esm/react-shared/ReactSharedInternals.js
+++ esm/react-shared/ReactSharedInternals.js
@@ -4,7 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import React from "../../index.js";
+import React from "../index.js";
 const ReactSharedInternals = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED; // Prevent newer renderers from RTE when used with older react package versions.
 // Current owner and dispatcher used to share the same ref,
 // but PR #14548 split them out to better support the react-debug-tools package.
